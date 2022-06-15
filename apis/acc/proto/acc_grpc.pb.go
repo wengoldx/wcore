59718d266bcc54c133a31e7bcac22c335ca087a1
jidi-J <j18041361158@163.com>
2022-06-15 12:33:27 +0800

    append get user prfile method

--- apis/acc/proto/acc_grpc.pb.go
+++ apis/acc/proto/acc_grpc.pb.go
@@ -24,6 +24,7 @@ const _ = grpc.SupportPackageIsVersion7
 type AccClient interface {
 	ViaToken(ctx context.Context, in *Token, opts ...grpc.CallOption) (*UUID, error)
 	GetProfSumms(ctx context.Context, in *UUIDS, opts ...grpc.CallOption) (*ProfSumms, error)
+	GetProfile(ctx context.Context, in *UUID, opts ...grpc.CallOption) (*Profile, error)
 }
 
 type accClient struct {
@@ -52,12 +53,22 @@ func (c *accClient) GetProfSumms(ctx context.Context, in *UUIDS, opts ...grpc.Ca
 	return out, nil
 }
 
+func (c *accClient) GetProfile(ctx context.Context, in *UUID, opts ...grpc.CallOption) (*Profile, error) {
+	out := new(Profile)
+	err := c.cc.Invoke(ctx, "/proto.Acc/GetProfile", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // AccServer is the server API for Acc service.
 // All implementations must embed UnimplementedAccServer
 // for forward compatibility
 type AccServer interface {
 	ViaToken(context.Context, *Token) (*UUID, error)
 	GetProfSumms(context.Context, *UUIDS) (*ProfSumms, error)
+	GetProfile(context.Context, *UUID) (*Profile, error)
 	mustEmbedUnimplementedAccServer()
 }
 
@@ -70,6 +81,9 @@ func (UnimplementedAccServer) ViaToken(context.Context, *Token) (*UUID, error) {
 }
 func (UnimplementedAccServer) GetProfSumms(context.Context, *UUIDS) (*ProfSumms, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetProfSumms not implemented")
+}
+func (UnimplementedAccServer) GetProfile(context.Context, *UUID) (*Profile, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetProfile not implemented")
 }
 func (UnimplementedAccServer) mustEmbedUnimplementedAccServer() {}
 
@@ -120,6 +134,24 @@ func _Acc_GetProfSumms_Handler(srv interface{}, ctx context.Context, dec func(in
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Acc_GetProfile_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(UUID)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(AccServer).GetProfile(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/proto.Acc/GetProfile",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(AccServer).GetProfile(ctx, req.(*UUID))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // Acc_ServiceDesc is the grpc.ServiceDesc for Acc service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -134,6 +166,10 @@ var Acc_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "GetProfSumms",
 			Handler:    _Acc_GetProfSumms_Handler,
+		},
+		{
+			MethodName: "GetProfile",
+			Handler:    _Acc_GetProfile_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
