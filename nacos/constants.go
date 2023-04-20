ffbe5b8ae855f2e4f51e18eb830e807f17500826
youhei <ping.yang@wengold.net>
2023-04-20 10:47:12 +0800

    Change nacos data ids

--- nacos/constants.go
+++ nacos/constants.go
@@ -47,19 +47,14 @@ const (
 
 // Nacos data id for wing/nacos
 const (
-	DID_ACC_ADMINS   = "dunyu.acc.admins"   // Group by BASIC, data id of sysmgr admins
-	DID_ACC_SALT     = "dunyu.acc.secure"   // Group by BASIC, data id of account secure salt
-	DID_ACC_EMAIL    = "dunyu.acc.email"    // Group by BASIC, data id of email sender settings
-	DID_ACC_SMS      = "dunyu.acc.sms"      // Group by BASIC, data id of sms sender settings
+	DID_ACC_CONFIGS  = "dunyu.acc.configs"  // Group by BASIC, data id of accservice cofnigs
 	DID_WX_AGENTS    = "dunyu.wx.agents"    // Group by BASIC, data id of wechat agents
 	DID_MIO_PATHS    = "dunyu.mio.paths"    // Group by BASIC, data id of minio source paths
-	DID_NTF_SENDER   = "dunyu.ntf.sender"   // Group by BASIC, data id of dingtalk gym watchdog nofitier
-	DID_NTF_PROPOSER = "dunyu.ntf.proposer" // Group by BASIC, data id of dingtalk suggestion nofitier
-	DID_NTF_WGPAY    = "dunyu.ntf.wgpay"    // Group by BASIC, data id of dingtalk wgpay nofitier
-	DID_NTF_ORDER    = "dunyu.ntf.order"    // Group by BASIC, data id of dingtalk trade order nofitier
+	DID_DTALK_NTFERS = "dunyu.dtalk.ntfers" // Group by BASIC, data id of dingtalk notifiers
 	DID_ES_AGENTS    = "dunyu.es.agents"    // Group by IFSC,  data id of elastic search agents
 	DID_MQTT_AGENTS  = "dunyu.mqtt.agents"  // Group by IFSC,  data id of mqtt agents
 	DID_OTA_BUILDS   = "dunyu.ota.builds"   // Group by BASIC, data id of all projects OTA informations
+	DID_API_ROUTERS  = "dunyu.api.routers"  // Group by BASIC, data id of swagger restful routers
 
 	// For GRPC certs content data ids
 	DID_CERTK_ACC  = "dunyu.cert.acc.key"  // Group by BASIC, data id of cert key of accservice for grpc
@@ -70,6 +65,18 @@ const (
 	DID_CERTP_PAY  = "dunyu.cert.pay.pem"  // Group by BASIC, data id of cert pem of wgpay for grpc
 	DID_CERTK_CHAT = "dunyu.cert.chat.key" // Group by BASIC, data id of cert key of wgchat for grpc
 	DID_CERTP_CHAT = "dunyu.cert.chat.pem" // Group by BASIC, data id of cert pem of wgchat for grpc
+
+	// @DEPRECATED use DID_ACC_CONFIGS insted
+	DID_ACC_ADMINS = "dunyu.acc.admins" // Group by BASIC, data id of sysmgr admins
+	DID_ACC_SALT   = "dunyu.acc.secure" // Group by BASIC, data id of account secure salt
+	DID_ACC_EMAIL  = "dunyu.acc.email"  // Group by BASIC, data id of email sender settings
+	DID_ACC_SMS    = "dunyu.acc.sms"    // Group by BASIC, data id of sms sender settings
+
+	// @DEPRECATED use DID_DTALK_NTFERS insted
+	DID_NTF_SENDER   = "dunyu.ntf.sender"   // Group by BASIC, data id of dingtalk gym watchdog nofitier
+	DID_NTF_PROPOSER = "dunyu.ntf.proposer" // Group by BASIC, data id of dingtalk suggestion nofitier
+	DID_NTF_WGPAY    = "dunyu.ntf.wgpay"    // Group by BASIC, data id of dingtalk wgpay nofitier
+	DID_NTF_ORDER    = "dunyu.ntf.order"    // Group by BASIC, data id of dingtalk trade order nofitier
 
 	// @DEPRECATED use DID_OTA_BUILDS insted
 	DID_OTA_STOR  = "dunyu.ota.store" // Group by BASIC, data id of store OTA
